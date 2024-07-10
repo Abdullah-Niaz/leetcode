26e90ffb3cc67006f457f07a030d2ad2a7bbecbc
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-10 12:10:27 +0500

    optimal apprach

--- 26. Remove Duplicates from Sorted Array/optimal.cpp
+++ 26. Remove Duplicates from Sorted Array/optimal.cpp
@@ -4,20 +4,17 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> st;
-
-        for(int i = 0 ;i < nums.size(); i++){
-            st.insert(nums[i]);
-        }   
-
-        int index = 0 ;
-        for( auto it:st){
-            nums[index]  = it;
-            index++;
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++){
+            if(nums[i]  != nums[j]){
+                nums[i+1] = nums[j];
+                i++;
+            }
         }
-        return index;
+        return i + 1;
     }
 };
+
 int main(){
     Solution solution;
     std::vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
